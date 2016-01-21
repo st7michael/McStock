fffb8d7c46c26ac0a66ba0af92741fbd9e65c530
Yiqiao <Yiqiao@yqiao-pro.local>
2016-01-21 13:37:45 +0800

    base day1 in mac

---
+++ McStock/McStock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  McStock
+//
+//  Created by Yiqiao on 16/1/21.
+//  Copyright © 2016年 Yiqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
